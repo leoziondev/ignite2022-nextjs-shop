b7ac70211506b99f918da5a8ae718b87b391ad00
LeoZion <leozionsuporte@gmail.com>
2022-10-04 17:07:16 -0300

    design home and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,39 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home";
+import Image from "next/future/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 8,
-  border: 0,
-  padding: '8px 12px',
-  color: 'white',
-  cursor: 'pointer',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import shirt1 from '../assets/shirt/shirt-01.png'
+import shirt2 from '../assets/shirt/shirt-02.png'
+import shirt3 from '../assets/shirt/shirt-03.png'
+import shirt4 from '../assets/shirt/shirt-04.png'
+import shirt5 from '../assets/shirt/shirt-05.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Hello</span> NextJS</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
