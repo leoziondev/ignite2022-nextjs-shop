a25c5f5b77ce9f4245ba8487dd348b07e2af33d2
LeoZion <leozionsuporte@gmail.com>
2022-10-04 14:39:04 -0300

    config colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from "../styles"
 
 const Button = styled('button', {
-  backgroundColor: '$brand',
+  backgroundColor: '$green500',
   borderRadius: 8,
   border: 0,
   padding: '8px 12px',
