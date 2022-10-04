a22d2f9e2ed279ac5d99677dc216c39e6a476c6b
LeoZion <leozionsuporte@gmail.com>
2022-10-04 15:01:58 -0300

    add app styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from 'next/app'
+import Image from 'next/future/image'
 import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
