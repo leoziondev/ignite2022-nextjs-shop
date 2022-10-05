6b62a0c25030538993c4ab6f0fd95cb8ef78e540
LeoZion <leozionsuporte@gmail.com>
2022-10-05 11:36:04 -0300

    design success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>Uhuuul <strong>Jhon Doe</strong>, sua <strong>Camiseta X</strong> já está sendo enviada para sua casa!</p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
